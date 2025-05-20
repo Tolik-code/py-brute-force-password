fbbb52033279681c27af9ef55c0e01c138ec082c
Tolik-code <72889479+Tolik-code@users.noreply.github.com>
2025-05-20 21:51:39 +0300

    Optimized and added multiprocessing async

--- app/main.py
+++ app/main.py
@@ -1,8 +1,10 @@
+from multiprocessing import Pool, cpu_count
 import time
 from hashlib import sha256
+from itertools import product
+from typing import Iterable
 
-
-PASSWORDS_TO_BRUTE_FORCE = [
+PASSWORDS_TO_BRUTE_FORCE = {
     "b4061a4bcfe1a2cbf78286f3fab2fb578266d1bd16c414c650c5ac04dfc696e1",
     "cf0b0cfc90d8b4be14e00114827494ed5522e9aa1c7e6960515b58626cad0b44",
     "e34efeb4b9538a949655b788dcb517f4a82e997e9e95271ecd392ac073fe216d",
@@ -12,21 +14,37 @@ PASSWORDS_TO_BRUTE_FORCE = [
     "5e6bc66ee1d2af7eb3aad546e9c0f79ab4b4ffb04a1bc425a80e6a4b0f055c2e",
     "1273682fa19625ccedbe2de2817ba54dbb7894b7cefb08578826efad492f51c9",
     "7e8f0ada0a03cbee48a0883d549967647b3fca6efeb0a149242f19e4b68d53d6",
-    "e5f3ff26aa8075ce7513552a9af1882b4fbc2a47a3525000f6eb887ab9622207",
-]
-
+    "e5f3ff26aa8075ce7513552a9af1882b4fbc2a47a3525000f6eb887ab9622207"
+}
 
 def sha256_hash_str(to_hash: str) -> str:
     return sha256(to_hash.encode("utf-8")).hexdigest()
 
+def check_combination(combination: Iterable) -> None | str:
+    current_guess = ''.join(map(str, combination))
+    hashed = sha256_hash_str(current_guess)
+    if hashed in PASSWORDS_TO_BRUTE_FORCE:
+        print(f"[+] Found match: {current_guess} -> {hashed}")
+        return current_guess
+    return None
 
 def brute_force_password() -> None:
-    pass
-
+    guessed_passwords = []
+    with Pool(cpu_count()) as pool:
+        for result in pool.imap_unordered(
+                check_combination,
+                product(range(10), repeat=8),
+                chunksize=10000
+        ):
+            if result is not None:
+                guessed_passwords.append(result)
+                if len(guessed_passwords) >= 10:
+                    pool.terminate()
+                    break
+    print(guessed_passwords, "guessed_passwords")
 
 if __name__ == "__main__":
     start_time = time.perf_counter()
     brute_force_password()
     end_time = time.perf_counter()
-
     print("Elapsed:", end_time - start_time)
